672913cb6d8881edb610fd5618d301b32ddc4a7f
Dae In <daein7076@gmail.com>
2019-05-19 00:18:08 +0900

    layouts with pug

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from 'morgan'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
+
 import userRouter from './routers/userRouter'
 import videoRouter from './routers/videoRouter'
 import globalRouter from './routers/globalRouter'
@@ -10,6 +11,7 @@ import routes from './routes'
 
 const app = express()
 
+app.set('view engine', 'pug')
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
