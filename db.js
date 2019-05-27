3ee2cad7662e938c8bd38eab737afda092c89c0a
DI-Kim <daein7076@gmail.com>
2019-05-28 08:53:36 +0900

    home controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id:3243883,
+    title: 'Video awesome',
+    description: 'This is something I love',
+    views: 24,
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: 'Dae In',
+      email: 'daein7076@naver.com'
+    }
+  },
+  {
+    id:123124,
+    title: 'Video super',
+    description: 'This is something I love',
+    views: 24,
+    videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    creator: {
+      id: 121212,
+      name: 'Dae In',
+      email: 'daein7076@naver.com'
+    }
+  },
+  {
+    id:13442,
+    title: 'Video nice',
+    description: 'This is something I love',
+    views: 24,
+    videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    creator: {
+      id: 121212,
+      name: 'Dae In',
+      email: 'daein7076@naver.com'
+    }
+  },
+  {
+    id:97972,
+    title: 'Video perfect',
+    description: 'This is something I love',
+    views: 24,
+    videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    creator: {
+      id: 121212,
+      name: 'Dae In',
+      email: 'daein7076@naver.com'
+    }
+  }
+]
