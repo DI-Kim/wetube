2f6765015d28fb13316812e31d94e2eda5777e93
DI-Kim <daein7076@gmail.com>
2019-06-01 17:36:26 +0900

    introduction to Webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+// 예전 자바스크립트를 써야됨
+const path = require("path"); //  = import path from "path"
+
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+const config = {
+  entry: ENTRY_FILE,
+  output: {
+    path: OUTPUT_DIR,
+    filename: "[name.[format]"
+  }
+};
+
+module.exports = config;
