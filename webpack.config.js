b9c644cc113860241a67a09a64c55b72737aadf4
DI-Kim <daein7076@gmail.com>
2019-06-01 17:54:30 +0900

    styles with Webpack part one

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,35 @@
 // 예전 자바스크립트를 써야됨
 const path = require("path"); //  = import path from "path"
+const ExtractCSS = require("extract-text-webpack-plugin");
 
+const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
   entry: ENTRY_FILE,
+  mode: MODE,
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: ExtractCSS.extract([
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "postcss-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ])
+      }
+    ]
+  },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name.[format]"
+    filename: "[name].[format]"
   }
 };
 
