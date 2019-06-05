f17ac080d652762e48c2574497298bfa8084a8bf
DI-Kim <daein7076@gmail.com>
2019-06-05 19:46:26 +0900

    adding Creator to Video

--- models/Comment.js
+++ models/Comment.js
@@ -8,12 +8,16 @@ const CommentSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now
-  }
+  },
   // 코멘트에 video의 아이디를 저장하여 어느 비디오에 달린 댓글인지 알 수 있음
   // video: {
   //   type: mongoose.Schema.Types.ObjectId,
   //   ref: "Video"
   // }
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
 });
 
 const model = mongoose.model("Comment", CommentSchema);
